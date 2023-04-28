614a2bfda541194b6cecbe90acf8677f3580c94b
gabrielblanco2007 <gabrieltarazonablanco@gmail.com>
2023-04-27 21:33:49 -0500

    actualizacion operaciones basicas matematicas

---
+++ Calculadora.cpp
@@ -0,0 +1,12 @@
+ #include <iostream>
+
+ using namespace std; 
+
+
+int main()
+{
+    cout << "Calculadora Gabriel#67" << endl << endl;
+    cout << "Porfavor escribe la operacion a ejecutar. Ejemplo:  a+b | a-b | a*b | a/b "
+        << endl;
+    return 0;
+}
